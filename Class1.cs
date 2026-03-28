4a1cc954f4cac22a985b7cb9fa8f2d11bbad0413
unknown <@ngknn.local>
2026-03-28 08:25:50 +0300

    First commit

---
+++ Class1.cs
@@ -0,0 +1,142 @@
+﻿namespace Geometrics
+{
+
+    public class Circle
+    {
+        public double Radius { get; private set; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public double GetArea()
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+
+        public double GetPerimeter()
+        {
+            return 2 * Math.PI * Radius;
+        }
+
+        public double GetDiameter()
+        {
+            return 2 * Radius;
+        }
+    }
+    public class Square
+    {
+        public double SideLength { get; private set; }
+
+        public Square(double sideLength)
+        {
+            SideLength = sideLength;
+        }
+
+        public double GetArea()
+        {
+            return Math.Pow(SideLength, 2);
+        }
+
+        public double GetPerimeter()
+        {
+            return 4 * SideLength;
+        }
+
+        public double GetDiagonal()
+        {
+            return SideLength * Math.Sqrt(2);
+        }
+    }
+
+    public class Triangle
+    {
+        public double SideA { get; private set; }
+        public double SideB { get; private set; }
+        public double SideC { get; private set; }
+
+        public Triangle(double sideA, double sideB, double sideC)
+        {
+            SideA = sideA;
+            SideB = sideB;
+            SideC = sideC;
+        }
+
+        public double GetPerimeter()
+        {
+            return SideA + SideB + SideC;
+        }
+
+        public double GetArea()
+        {
+
+            double s = GetPerimeter() / 2;
+            return Math.Sqrt(s * (s - SideA) * (s - SideB) * (s - SideC));
+        }
+
+        public (double AngleA, double AngleB, double AngleC) GetAngles()
+        {
+
+            double angleA = Math.Acos((Math.Pow(SideB, 2) + Math.Pow(SideC, 2) - Math.Pow(SideA, 2)) / (2 * SideB * SideC));
+            double angleB = Math.Acos((Math.Pow(SideA, 2) + Math.Pow(SideC, 2) - Math.Pow(SideB, 2)) / (2 * SideA * SideC));
+            double angleC = Math.Acos((Math.Pow(SideA, 2) + Math.Pow(SideB, 2) - Math.Pow(SideC, 2)) / (2 * SideA * SideB));
+            return (angleA * (180 / Math.PI), angleB * (180 / Math.PI), angleC * (180 / Math.PI));
+        }
+    }
+    public class Rectangle
+    {
+        public double Length { get; private set; }
+        public double Width { get; private set; }
+
+        public Rectangle(double length, double width)
+        {
+            Length = length;
+            Width = width;
+        }
+
+        public double GetArea()
+        {
+            return Length * Width;
+        }
+
+        public double GetPerimeter()
+        {
+            return 2 * (Length + Width);
+        }
+
+        public double GetDiagonal()
+        {
+            return Math.Sqrt(Math.Pow(Length, 2) + Math.Pow(Width, 2));
+        }
+    }
+
+    // Класс для трапеции
+    public class Trapezoid
+    {
+        public double BaseA { get; private set; }
+        public double BaseB { get; private set; }
+        public double SideC { get; private set; }
+        public double SideD { get; private set; }
+        public double Height { get; private set; }
+
+        public Trapezoid(double baseA, double baseB, double sideC, double sideD, double height)
+        {
+            BaseA = baseA;
+            BaseB = baseB;
+            SideC = sideC;
+            SideD = sideD;
+            Height = height;
+        }
+
+        public double GetArea()
+        {
+            return ((BaseA + BaseB) / 2) * Height;
+        }
+
+        public double GetPerimeter()
+        {
+            return BaseA + BaseB + SideC + SideD;
+        }
+    }
+}
